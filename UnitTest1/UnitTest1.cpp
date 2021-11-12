25db59ae20d9ce0dda4f631370cd796a6950496d
olehhordii <oleh.hordii.ik.2021@lpnu.ua>
2021-11-12 23:25:26 +0200

    first commit master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.2_iter/lab6.2_iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 5;
+			int a[n] = {-2,3,5,8,-22};
+			int c;
+			c = Count(a, n);
+			Assert::AreEqual(2, c);
+		}
+	};
+}
---
+++ lab6.2_iter/lab6.2_iter.cpp
@@ -0,0 +1,43 @@
+﻿// lab6.2_iter.cpp 
+
+using namespace std;
+#include <iostream>
+#include <iomanip>
+void CreateArray(int* a, const int size, const int Low, const int High) {
+	for (int i = 0; i < size; i++) {
+		a[i] = Low + rand() % (High - Low + 1);
+	}
+}
+void PrintArray(int* a, const int size) {
+	cout << " a[" << size << "] = " << "{";
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << a[i];
+	}
+	cout << " }" << endl;
+}
+int Count(int* a, const int size) {
+	int coun = 0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] % 2 != 0) {
+			coun++;
+		}
+	}
+	return coun;
+}
+
+int main()
+{  
+	srand((unsigned)time(NULL));
+    const int n=7;
+    int a[n];
+	int Low,High;
+	cout << "Enter low value "; cin >> Low;
+	cout << endl;
+	cout << "Enter high value "; cin >> High;
+	cout << "Array: ";
+	CreateArray(a, n, Low, High);
+	PrintArray(a, n);
+
+	cout << "number of neparnuh elements = " << Count(a, n) << endl;
+	return 0;
+}
